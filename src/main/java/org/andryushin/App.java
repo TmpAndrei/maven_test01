2ffd1db75e8856a3aa698614f71bd3d248e0f966
locadmin <locadmin@linux-oqj1.site>
2014-09-30 09:40:10 +0300

    Complete

--- src/main/java/org/andryushin/App.java
+++ src/main/java/org/andryushin/App.java
@@ -1,13 +1,15 @@
 package org.andryushin;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public String reverseString(String src) {
+        return new StringBuilder(src).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        final String str = "Hello!";
+        App appObject = new App();
+
+        System.out.println("The reverse string of \"" + str + "\" is \"" +
+                appObject.reverseString(str) + "\"");
     }
 }
